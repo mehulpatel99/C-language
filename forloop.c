795a570573fad632a4bc723444f8ad97d8fd818c
mehulpatel99 <138190866+mehulpatel99@users.noreply.github.com>
2023-07-12 13:46:21 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    /* The loop goes while i < 10, and i increases by one every loop*/
+
+    for ( i = 897; i < 972; i++ ) {
+        printf( " %d ",i);
+    }
+}
