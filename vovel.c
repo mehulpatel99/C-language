52218d4f64dae16e51bd41c6151ba6974b5b0a58
mehulpatel99 <138190866+mehulpatel99@users.noreply.github.com>
2023-07-12 12:19:19 +0530

    Add files via upload

---
+++ vovel.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+{
+	
+     char week;
+    
+	
+	printf("enter your latters");
+	scanf("%c",&week);
+	printf("your day is:");
+	
+	
+	switch(week)
+	{
+		case 'a':
+		printf("vovel");
+		break;
+		
+		case 'e':
+		printf("vovel");
+		break;
+		
+		case 'i':
+		printf("vovel");
+		break;
+		
+		case 'o':
+		printf("vovel");
+		break;
+		
+		case 'u':
+		printf("vovel");
+		break;
+		
+		
+		
+		default:
+			printf("constant");
+			
+		}
+	}
