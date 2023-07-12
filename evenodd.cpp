5f27b659e135d8f15f9ab9e000c6675b7d749f58
mehulpatel99 <138190866+mehulpatel99@users.noreply.github.com>
2023-07-12 12:13:42 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("entere the integer number\n");
+	scanf(" %d",&a);
+	
+	 
+	 if(a%2 ==0)
+	 {
+	 	printf("%d is even",a);
+	 }
+	else
+	{
+		printf("%d is odd",a);
+	}
+	
+}
